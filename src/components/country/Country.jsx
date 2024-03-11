4178fb7ab8241027a0e52d780ef19f3925b35a81
Asad9340 <asadulimran1999@gmail.com>
2024-03-12 02:02:48 +0600

    added visited button togol

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
+
 function Country({ country }) {
-  console.log(country);
+  const [visited , setVisited] = useState(false)
   const { flags, name, capital, continents, area, independent } = country;
+
+
+  const handelClick = () => {
+    setVisited(!visited);
+  }
+
   return (
     <div className="my-2 p-4 border border-gray-700 rounded-xl hover:bg-gray-800 hover:text-white hover:duration-300">
       <div className="flex justify-center">
@@ -16,9 +24,10 @@ function Country({ country }) {
       <p>Continents: {continents}</p>
       <p>Area: {area} square Kilometer</p>
       <p>Independent: {independent ? 'Yes' : 'No'}</p>
+      <p>{ visited?"I have visited this place":"Not visited . I will visit it soon"}</p>
       <div className="flex gap-4 mt-4">
-        <button className="py-2 px-6 bg-indigo-700 hover:bg-indigo-800 border border-indigo-800 hover:border hover:border-red-900 text-white rounded-lg outline-none">
-          Visited
+        <button onClick={handelClick} className="py-2 px-6 bg-indigo-700 hover:bg-indigo-800 border border-indigo-800 hover:border hover:border-red-900 text-white rounded-lg outline-none">
+          {visited?"Visited":'Going'}
         </button>
         <button className="py-2 px-6 bg-indigo-700 hover:bg-indigo-800 border border-indigo-800 hover:border hover:border-red-900 text-white rounded-lg outline-none">
           Mark
