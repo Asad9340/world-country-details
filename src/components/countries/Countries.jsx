06f30075d912004316ef5dbd334187ea048162a8
Asad9340 <asadulimran1999@gmail.com>
2024-03-12 02:29:20 +0600

    added click counter

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -2,6 +2,11 @@ import { useEffect, useState } from 'react';
 import Country from '../country/Country';
 export default function Countries() {
   const [data, setData] = useState([]);
+  const [count , setCount] = useState(0);
+
+  const handleCount = () => {
+    setCount(count + 1);
+  }
   useEffect(() => {
     const handelData = async () => {
       const res = await fetch('https://restcountries.com/v3.1/all');
@@ -12,10 +17,19 @@ export default function Countries() {
     handelData();
   }, []);
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mx-4 md:mx-8 lg:mx-10">
-      {data.map((country, index) => (
-        <Country key={index} country={country}></Country>
-      ))}
+    <div>
+      <h3 className="text-3xl font-semibold my-6 text-center">
+        Click Counter : <span className='text-red-700 font-extrabold'>{count}</span>
+      </h3>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mx-4 md:mx-8 lg:mx-10">
+        {data.map((country, index) => (
+          <Country
+            key={index}
+            country={country}
+            handleCount={handleCount}
+          ></Country>
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,16 +1,19 @@
 import { useState } from "react";
 
-function Country({ country }) {
-  const [visited , setVisited] = useState(false)
+function Country({ country, handleCount }) {
+  const [visited, setVisited] = useState(false);
   const { flags, name, capital, continents, area, independent } = country;
-
 
   const handelClick = () => {
     setVisited(!visited);
-  }
+  };
 
   return (
-    <div className="my-2 p-4 border border-gray-700 rounded-xl hover:bg-gray-800 hover:text-white hover:duration-300">
+    <div
+      className={`my-2 p-4 border border-gray-700 rounded-xl hover:bg-gray-800 hover:text-white hover:duration-300 ${
+        visited ? 'bg-gray-300' : ''
+      }`}
+    >
       <div className="flex justify-center">
         <img
           className="rounded-lg object-contain mb-4"
@@ -24,15 +27,25 @@ function Country({ country }) {
       <p>Continents: {continents}</p>
       <p>Area: {area} square Kilometer</p>
       <p>Independent: {independent ? 'Yes' : 'No'}</p>
-      <p>{ visited?"I have visited this place":"Not visited . I will visit it soon"}</p>
+      <p>
+        {visited
+          ? 'I have visited this place'
+          : 'Not visited . I will visit it soon'}
+      </p>
       <div className="flex gap-4 mt-4">
-        <button onClick={handelClick} className="py-2 px-6 bg-indigo-700 hover:bg-indigo-800 border border-indigo-800 hover:border hover:border-red-900 text-white rounded-lg outline-none">
-          {visited?"Visited":'Going'}
+        <button
+          onClick={handelClick}
+          className="py-2 px-6 bg-indigo-700 hover:bg-indigo-800 border border-indigo-800 hover:border hover:border-red-900 text-white rounded-lg outline-none"
+        >
+          {visited ? 'Visited' : 'Going'}
         </button>
         <button className="py-2 px-6 bg-indigo-700 hover:bg-indigo-800 border border-indigo-800 hover:border hover:border-red-900 text-white rounded-lg outline-none">
           Mark
         </button>
-        <button className="py-2 px-6 bg-indigo-700 hover:bg-indigo-800 border border-indigo-800 hover:border hover:border-red-900 text-white rounded-lg outline-none">
+        <button
+          onClick={handleCount}
+          className="py-2 px-6 bg-indigo-700 hover:bg-indigo-800 border border-indigo-800 hover:border hover:border-red-900 text-white rounded-lg outline-none"
+        >
           Count
         </button>
       </div>
